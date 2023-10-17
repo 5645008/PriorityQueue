dffe79ee15332dd7ce630ce12dadf69ea37e67fe
5645008 <5645008@stu.kmu.ac.kr>
2023-10-17 10:32:39 +0900

    히프정렬-실습

--- PriorityQueue/FileName.cpp
+++ PriorityQueue/FileName.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 #define MAX_ELEMENT 200
+#define SIZE 17
 
 typedef struct
 {
@@ -14,8 +15,6 @@ typedef struct
 	int heap_size;
 } HeapType;
 
-//노드의 이동횟수를 세는 정수
-int node = 0;
 
 // 생성 함수
 HeapType* create()
@@ -35,21 +34,17 @@ void insert_max_heap(HeapType* h, element item)
 	int i;
 	i = ++(h->heap_size);
 	h->heap[h->heap_size] = item;
-	for (int j = 1; j <= h->heap_size; j++) {
-		printf("%d ", h->heap[j].key);
-	}
-	printf("\n");
 
 	while ((i != 1) && (item.key > h->heap[i / 2].key)) {
 		h->heap[i] = h->heap[i / 2];
 		h->heap[i / 2] = item;
 		i /= 2;
-		node++;
-		for (int j = 1; j <= h->heap_size; j++) {
-			printf("%d ", h->heap[j].key);
-		}
-		printf("\n");
+		
 	}
+	for (int j = 1; j <= h->heap_size; j++) {
+		printf("%d ", h->heap[j].key);
+	}
+	printf("\n");
 	h->heap[i] = item;
 }
 
@@ -65,10 +60,6 @@ element delete_max_heap(HeapType* h)
 	
 	parent = 1;
 	child = 2;
-	for (int j = 1; j <= h->heap_size; j++) {
-		printf("%d ", h->heap[j].key);
-	}
-	printf("\n");
 
 	while (child <= h->heap_size)
 	{
@@ -82,115 +73,43 @@ element delete_max_heap(HeapType* h)
 		h->heap[child] = temp;
 		parent = child;
 		child *= 2;
-		for (int j = 1; j <= h->heap_size; j++) {
-			printf("%d ", h->heap[j].key);
-		}
-		printf("\n");
-		node++;
+		
 	}
+
 	h->heap[parent] = temp;
+	for (int j = 1; j <= h->heap_size; j++) {
+		printf("%d ", h->heap[j].key);
+	}
+	printf("\n");
 	return item;
 }
 
-//트리의 높이
-int height(HeapType* h) {
-	int start = 1;
-	int height = 1;
-	while (h->heap_size >= start*2) {
-		start = start * 2;
-		height++;
-	}
-	return height;
-}
 
-//히프를 루트에서부터 레벨별로 출력
-void level_print(HeapType* h)
+void heap_sort(element a[], int n)
 {
-	int j = 1;
-	int now = 0;
-	for (int i = 1; i <=height(h) ; i++) {
-		printf("[%d] ",i);
-		now = now + pow(2, i - 1);
-		for (; j <= now; j++) {
-			if (j > h->heap_size) {
-				break;
-			}
-			printf("%d ", h->heap[j].key);
-		}
-		printf("\n");
+	int i;
+	HeapType* h;
+
+	h = create();
+	init(h);
+	for (i = 0; i < n; i++) {
+		insert_max_heap(h, a[i]);
 	}
-	printf("\n");
-	
+	for (i = (n - 1); i >= 0; i--) {
+		a[i] = delete_max_heap(h);
+	}
+	free(h);
 }
 
-//입력시 생기는 엔터 받아주기
-void Clear(void) {
-	while (getchar() != '\n');
-}
 
 int main(void)
 {
 
-	HeapType* heap;
-	heap = create();
-	init(heap);
-
-	//트리 값 넣기
-	int a[10] = { 90, 89, 70, 36, 75, 63, 65, 21, 18, 15 };
-	for (int i = 0; i < 10; i++) {
-		heap->heap[i+1].key = a[i];
+	element list[SIZE] = { 34, 12, 76, 59, 32, 55, 88, 26, 16, 79, 34, 85, 29, 78, 41, 56, 86 };
+	heap_sort(list, SIZE);
+	for (int i = 0; i < SIZE; i++) {
+		printf("%d ", list[i].key);
 	}
-	heap->heap_size = 10;
-
-
-	//메뉴
-	printf("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ\n");
-	printf("| i   : 노드 추가   |\n");
-	printf("| d   : 노드 삭제   |\n");
-	printf("| p   : 레벨별 출력 |\n");
-	printf("| c   : 종료        |\n");
-	printf("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ\n");
-	printf("\n\n");
-
-	element number;
-	char command;
-	do {
-		printf("메뉴 입력: ");
-		command = getchar();
-
-		switch (command) {
-		//삽입(i)입력
-		case 'i':
-		{
-			printf("추가할 값 입력: ");
-			scanf_s("%d", &number);
-			Clear();
-			insert_max_heap(heap, number);
-			printf("노드가 이동된 횟수: %d\n\n", node);
-			node = 0;
-			break;
-		}
-		//삭제(d)입력
-		case 'd':
-		{
-			Clear();
-			delete_max_heap(heap);
-			printf("노드가 이동된 횟수: %d\n\n", node);
-			node = 0;
-			break;
-		}
-		//레벨별 출력(p)입력
-		case 'p':
-		{
-			Clear();
-			printf("트리 레벨별 출력\n");
-			level_print(heap);
-			break;
-		}
-		}
-		//c가 입력되면 do-while문 종료
-	} while (command != 'c');
-
-	free(heap);
+	printf("\n");
 	return 0;
 }
