b6309b01bc062acfc300931272fc11f739dc1a83
5645008 <5645008@stu.kmu.ac.kr>
2023-10-17 10:53:06 +0900

    허프만-실습-2

--- PriorityQueue/FileName.cpp
+++ PriorityQueue/FileName.cpp
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #define MAX_ELEMENT 200
+
+typedef struct TreeNode
+{
+	int weight;
+	char ch;
+	struct TreeNode* left;
+	struct TreeNode* right;
+} TreeNode;
 
 typedef struct
 {
+	TreeNode* ptree;
+	char ch;
 	int key;
 } element;
 
@@ -14,183 +23,146 @@ typedef struct
 	int heap_size;
 } HeapType;
 
-//노드의 이동횟수를 세는 정수
-int node = 0;
-
 // 생성 함수
 HeapType* create()
 {
 	return (HeapType*)malloc(sizeof(HeapType));
 }
-
 // 초기화 함수
 void init(HeapType* h)
 {
 	h->heap_size = 0;
 }
-
 // 삽입 함수
-void insert_max_heap(HeapType* h, element item)
+void insert_min_heap(HeapType* h, element item)
 {
 	int i;
 	i = ++(h->heap_size);
-	h->heap[h->heap_size] = item;
+	while ((i != 1) && (item.key < h->heap[i / 2].key)) {
+		h->heap[i] = h->heap[i / 2];
+		i /= 2;
+	}
+	h->heap[i] = item;
 	for (int j = 1; j <= h->heap_size; j++) {
 		printf("%d ", h->heap[j].key);
 	}
 	printf("\n");
-
-	while ((i != 1) && (item.key > h->heap[i / 2].key)) {
-		h->heap[i] = h->heap[i / 2];
-		h->heap[i / 2] = item;
-		i /= 2;
-		node++;
-		for (int j = 1; j <= h->heap_size; j++) {
-			printf("%d ", h->heap[j].key);
-		}
-		printf("\n");
-	}
-	h->heap[i] = item;
 }
-
 // 삭제 함수
-element delete_max_heap(HeapType* h)
+element delete_min_heap(HeapType* h)
 {
 	int parent, child;
 	element item, temp;
 
 	item = h->heap[1];
-	h->heap[1].key = h->heap[h->heap_size].key;
 	temp = h->heap[(h->heap_size)--];
-	
 	parent = 1;
 	child = 2;
-	for (int j = 1; j <= h->heap_size; j++) {
-		printf("%d ", h->heap[j].key);
-	}
-	printf("\n");
-
 	while (child <= h->heap_size)
 	{
-		
-		if ((child < h->heap_size) &&
-			(h->heap[child].key) < h->heap[child + 1].key)
+		if ((child > h->heap_size) &&
+			(h->heap[child].key) > h->heap[child + 1].key)
 			child++;
-		if (temp.key >= h->heap[child].key) break;
-
+		if (temp.key < h->heap[child].key) break;
 		h->heap[parent] = h->heap[child];
-		h->heap[child] = temp;
 		parent = child;
 		child *= 2;
-		for (int j = 1; j <= h->heap_size; j++) {
-			printf("%d ", h->heap[j].key);
-		}
-		printf("\n");
-		node++;
 	}
 	h->heap[parent] = temp;
 	return item;
 }
-
-//트리의 높이
-int height(HeapType* h) {
-	int start = 1;
-	int height = 1;
-	while (h->heap_size >= start*2) {
-		start = start * 2;
-		height++;
-	}
-	return height;
-}
-
-//히프를 루트에서부터 레벨별로 출력
-void level_print(HeapType* h)
+// 이진 트리 생성 함수
+TreeNode* make_tree(TreeNode* left, TreeNode* right)
 {
-	int j = 1;
-	int now = 0;
-	for (int i = 1; i <=height(h) ; i++) {
-		printf("[%d] ",i);
-		now = now + pow(2, i - 1);
-		for (; j <= now; j++) {
-			if (j > h->heap_size) {
-				break;
-			}
-			printf("%d ", h->heap[j].key);
-		}
-		printf("\n");
-	}
+	TreeNode* node = (TreeNode*)malloc(sizeof(TreeNode));
+	node->left = left;
+	node->right = right;
+	return node;
+}
+// 이진 트리 제거 함수
+void destroy_tree(TreeNode* root)
+{
+	if (root == NULL) return;
+	destroy_tree(root->left);
+	destroy_tree(root->right);
+	free(root);
+}
+int is_leaf(TreeNode* root)
+{
+	return !(root->left) && !(root->right);
+}
+void print_array(int codes[], int n)
+{
+	for (int i = 0; i < n; i++)
+		printf("%d", codes[i]);
 	printf("\n");
-	
 }
 
-//입력시 생기는 엔터 받아주기
-void Clear(void) {
-	while (getchar() != '\n');
+void print_codes(TreeNode* root, int codes[], int top)
+{
+	// 1을 저장하고 순환호출 
+	if (root->left)
+	{
+		codes[top] = 1;
+		print_codes(root->left, codes, top + 1);
+	}
+
+	// 0을 저장하고 순환호출 
+	if (root->right)
+	{
+		codes[top] = 0;
+		print_codes(root->right, codes, top + 1);
+	}
+
+	// 단말노드이면 코드를 출력 
+	if (is_leaf(root))
+	{
+		printf("%c: ", root->ch);
+		print_array(codes, top);
+	}
+}
+// 허프만 코드 생성 함수
+void huffman_tree(int freq[], char ch_list[], int n)
+{
+	int i;
+	TreeNode* node, * x;
+	HeapType* heap;
+	element e, e1, e2;
+	int codes[100];
+	int top = 0;
+
+	heap = create();
+	init(heap);
+	for (i = 0; i < n; i++)
+	{
+		node = make_tree(NULL, NULL);
+		e.ch = node->ch = ch_list[i];
+		e.key = node->weight = freq[i];
+		e.ptree = node;
+		insert_min_heap(heap, e);
+	}
+	for (i = 1; i < n; i++)
+	{
+		// 최소값을 가지는 두개의 노드를 삭제
+		e1 = delete_min_heap(heap);
+		e2 = delete_min_heap(heap);
+		// 두개의 노드를 합침
+		x = make_tree(e1.ptree, e2.ptree);
+		e.key = x->weight = e1.key + e2.key;
+		e.ptree = x;
+		printf("%d+%d->%d \n", e1.key, e2.key, e.key);
+		insert_min_heap(heap, e);
+	}
+	e = delete_min_heap(heap); // 최종 트리
+	print_codes(e.ptree, codes, top);
+	destroy_tree(e.ptree);
+	free(heap);
 }
 
 int main(void)
 {
-
-	HeapType* heap;
-	heap = create();
-	init(heap);
-
-	//트리 값 넣기
-	int a[10] = { 90, 89, 70, 36, 75, 63, 65, 21, 18, 15 };
-	for (int i = 0; i < 10; i++) {
-		heap->heap[i+1].key = a[i];
-	}
-	heap->heap_size = 10;
-
-
-	//메뉴
-	printf("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ\n");
-	printf("| i   : 노드 추가   |\n");
-	printf("| d   : 노드 삭제   |\n");
-	printf("| p   : 레벨별 출력 |\n");
-	printf("| c   : 종료        |\n");
-	printf("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ\n");
-	printf("\n\n");
-
-	element number;
-	char command;
-	do {
-		printf("메뉴 입력: ");
-		command = getchar();
-
-		switch (command) {
-		//삽입(i)입력
-		case 'i':
-		{
-			printf("추가할 값 입력: ");
-			scanf_s("%d", &number);
-			Clear();
-			insert_max_heap(heap, number);
-			printf("노드가 이동된 횟수: %d\n\n", node);
-			node = 0;
-			break;
-		}
-		//삭제(d)입력
-		case 'd':
-		{
-			Clear();
-			delete_max_heap(heap);
-			printf("노드가 이동된 횟수: %d\n\n", node);
-			node = 0;
-			break;
-		}
-		//레벨별 출력(p)입력
-		case 'p':
-		{
-			Clear();
-			printf("트리 레벨별 출력\n");
-			level_print(heap);
-			break;
-		}
-		}
-		//c가 입력되면 do-while문 종료
-	} while (command != 'c');
-
-	free(heap);
+	char ch_list[] = { 'a','e', 'i', 'o', 'u', 's', 't' };
+	int freq[] = { 10, 15, 12, 3, 4, 13, 1 };
+	huffman_tree(freq, ch_list, 7);
 	return 0;
 }
